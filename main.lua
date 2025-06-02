1c26a396029188e772e3688592d01ef78f82c9cb
TMaliz <tmalizia@csus.edu>
2025-06-01 20:16:13 -0700

    Changed ship and asteroids into objects.

--- main.lua
+++ main.lua
@@ -1,31 +1,63 @@
---Next step is to create unique asteroids and an iterator to draw a dynamic number of objects
+--Next step is to create bullet objects and mouse press functionality. Check for collisions between Bullets - Asteroids, Asteroids - Player
 
---This keeps the ship glued to the mouse
-function shipMouse()
-    mouseX, mouseY = love.mouse.getPosition()
-    shipVertices = {mouseX+10,mouseY+15,mouseX,mouseY-15,mouseX-10,mouseY+15}
+--START SHIP
+local Ship = {mouseX=0, mouseY=0, health=1, shipVertices = {0,0,0,0,0,0}}
+
+function Ship:new()
+    ship = {mouseX, mouseY, health, shipVertices}
+    self.__index = self
+    return setmetatable(ship, self)
 end
 
-local function asteroidShape()
-math.randomseed(os.time())
-return math.random(4,10)
+function Ship:shipMouse()
+    self.mouseX, self.mouseY = love.mouse.getPosition()
+    self.shipVertices = {
+                    self.mouseX+10,self.mouseY+15,
+                    self.mouseX,self.mouseY-15,
+                    self.mouseX-10,self.mouseY+15
+                    }
 end
+--END SHIP
+
+--START ASTEROIDS
+local Asteroid = {x = 50, y = 50, shape = 0}
+
+function Asteroid:new()
+    asteroid = {x, y, shape = self.createAsteroidShape()}
+    self.__index = self
+    return setmetatable(asteroid, self)
+end
+
+function Asteroid.createAsteroidShape()
+    math.randomseed(os.time())
+    return math.random(4,10)
+end
+--END ASTEROIDS
+asteroids = {} --List of asteroid objects
+--Needs iterator
+
+--START BULLETS
+
+--END BULLETS
+bullets = {} --List of bullet objects
+--needs iterator
 
 --One time setup
 function love.load()
-    mouseX, mouseY = 0
     love.mouse.setVisible(false)
-    ast = asteroidShape()
+    aste = Asteroid:new()
+    player = Ship:new()
+    player:shipMouse()
 end
 
 --updates each frame
 function love.update()
-    shipMouse()
+    player:shipMouse()
 end
 
 --Eventually need to use a table with a linked list+iterator to draw shapes...otherwise static asteroids
 function love.draw()
     love.graphics.setColor(1,1,0)
-    love.graphics.polygon("line", shipVertices)
-    love.graphics.circle("fill", 50,50,10,ast)
+    love.graphics.polygon("line", player.shipVertices)
+    love.graphics.circle("fill", aste.x, aste.y, 10, aste.shape)
 end
