20dc5dfa1c65c5a09b53a25600bec399922a704c
TMaliz <tmalizia@csus.edu>
2025-06-04 14:19:22 -0700

    Cleaned up draw and update by moving some features into separate functions. Shooting now has a cooldown.

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 --Next step is to mess with asteroids. Check for collisions between Bullets - Asteroids, Asteroids - Player
 
 --START SHIP
-local Ship = {mouseX=0, mouseY=0, health=1, shipVertices = {0,0,0,0,0,0}}
+local Ship = {mouseX=0, mouseY=0, health=1, shipVertices = {0,0,0,0,0,0}, shots = 0}
 
 function Ship:new()
-    ship = {mouseX, mouseY, health, shipVertices}
+    ship = {mouseX, mouseY, health, shipVertices, shots}
     self.__index = self
     return setmetatable(ship, self)
 end
@@ -46,23 +46,22 @@ end
 --END BULLETS
 bullets = {} --List of bullet objects
 
+
 --One time setup
 function love.load()
     love.mouse.setVisible(false)
     aste = Asteroid:new()
     player = Ship:new()
     player:shipMouse()
+    shots = 1 -- Preloads one shot
+    timer = 0 -- Used for updating bullets
 end
 
---updates each frame
+--Updates each frame
 function love.update()
     player:shipMouse()
-    if love.mouse.isDown(1) then
-        table.insert(bullets, Bullet:new(player.mouseX, player.mouseY))
-    end
-    for i = 1, #bullets do
-        bullets[i].y = bullets[i].y-1
-    end
+    bulletControl()
+    timer = timer + 1
 end
 
 --Draws objects in window
@@ -70,6 +69,28 @@ function love.draw()
     love.graphics.setColor(1,1,0)
     love.graphics.polygon("line", player.shipVertices)
     love.graphics.circle("fill", aste.x, aste.y, 10, aste.shape)
+    drawBullets()
+end
+
+
+--Controls for physics and interactions
+function bulletControl()
+      if love.mouse.isDown(1) and shots > 0 then
+        table.insert(bullets, Bullet:new(player.mouseX, player.mouseY))
+    shots = shots - 1
+    end
+    for i = 1, #bullets do
+        bullets[i].y = bullets[i].y-3
+    end
+    if timer >= 125 and shots == 0 then
+        shots = shots + 1
+        timer = 0
+    end
+end
+--end controls for physics and interactions
+
+-- Drawing functions
+function drawBullets()
     for i = 1, #bullets do
         love.graphics.circle("fill", bullets[i].x, bullets[i].y, 3)
     end
